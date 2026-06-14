2637b789b3036e78381fca99d5d5036b4c23c950
Tim Disney <tim@disnetdev.com>
2026-06-14 14:23:11 -0700

    switch feedback link to userinput.app

--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -560,8 +560,10 @@
       <span class="separator">·</span>
       <a href="mailto:abuse@skyreader.app">Report Abuse</a>
       <span class="separator">·</span>
-      <a href="https://github.com/disnet/skyreader/issues" target="_blank" rel="noopener noreferrer"
-        >Feedback</a
+      <a
+        href="https://userinput.app/#/s/did:plc:ra4jsemddo2ii4pn5jaf6x4v/3mobgsd6d5n27"
+        target="_blank"
+        rel="noopener noreferrer">Feedback</a
       >
     </div>
   </section>
